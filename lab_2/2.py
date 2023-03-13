dd6bd0b77a8b04edb084c1994210abb85bc665c1
Puntochange <vlogster989@gmail.com>
2023-03-13 09:20:08 +0100

    solve task 2

---
+++ lab_2/2.py
@@ -0,0 +1,4 @@
+# Wyświetl powyższy cytat 5 razy używając tylko jednej instrukcji print.
+
+print(" It's not personal. It's strictly business.\n", "It's not personal. It's strictly business.\n",
+      "It's not personal. It's strictly business.\n", "It's not personal. It's strictly business.\n", "It's not personal. It's strictly business.\n")
