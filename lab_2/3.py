9c771a1c1affe9f7e3fce7f033a76dc90e369d21
Eduard <goby145csgo@gmail.com>
2023-03-13 10:14:47 +0100

    solve task 3

---
+++ lab_2/3.py
@@ -0,0 +1,8 @@
+# Napisz program rysujący z gwiazdek * prostokąt o długości 10 i szerokości 5. Obok boków dodaj a i b.
+
+print("**********")
+print("*        *")
+print("*        * b")
+print("*        *")
+print("**********")
+print("    a")
