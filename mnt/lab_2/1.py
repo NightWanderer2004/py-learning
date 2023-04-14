ac68210e770eff42285954182e7f4d76688997e8
Lubos Rendek <web@linuxconfig.org>
2023-04-14 12:46:27 +0200

    add comments

--- mnt/lab_2/1.py
+++ mnt/lab_2/1.py
@@ -1,42 +1,32 @@
-# Zadanie 1
-# Uruchom i przeanalizuj poniższy kod programu. Wskaż, którą z powyższych metod
-# wykorzystano? Opisz krótko działanie zamieszczonego programu.
+import numpy as np             # Importujemy bibliotekę NumPy do operacji na liczbach i tablicach
+import matplotlib.pyplot as plt  # Importujemy bibliotekę Matplotlib do tworzenia wykresów
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-def f(x):
+def f(x):                      # Definiujemy funkcję, której pierwiastek będziemy szukać
     return x**2 + x - 1
 
-# Создаем массив значений аргумента функции от -1 до 1 с шагом 0.01
-t = np.arange(-1, 1, 0.01)
+t = np.arange(-1, 1, 0.01)     # Tworzymy tablicę t, w której przechowujemy wartości od -1 do 1 z krokiem 0.01
 
-# Строим график функции
-plt.plot(t, f(t))
-plt.grid(True)
-plt.xlabel('$x$')
-plt.ylabel('$y(x)$')
+plt.plot(t, f(t))              # Rysujemy wykres funkcji f(x) na przedziale [-1, 1]
+plt.grid(True)                 # Włączamy siatkę na wykresie
+plt.xlabel('$x$')              # Dodajemy etykietę osi x
+plt.ylabel('$y(x)$')           # Dodajemy etykietę osi y
 
-# Задаем начальные значения для поиска корня
-a = t[0]
-b = t[-1]
-e = 1e-4
-iMax = 100
+a = t[0]                       # Ustawiamy początkową wartość przedziału poszukiwania
+b = t[-1]                      # Ustawiamy końcową wartość przedziału poszukiwania
+e = 1e-4                       # Ustawiamy dokładność, z jaką chcemy znaleźć pierwiastek
+iMax = 100                     # Ustawiamy maksymalną liczbę iteracji
 
-# Используем метод бисекции для нахождения корня уравнения
-for k in range(iMax):
-    x = (a + b) / 2
-    if abs(f(x)) < e:
-        yo = f(x)
-        xo = x
-        break
-    elif f(a) * f(x) < 0:
-        b = x
-    else:
-        a = x
+for k in range(iMax):          # Uruchamiamy pętlę, która będzie szukać pierwiastka w danym przedziale
+    x = (a + b) / 2            # Obliczamy środek przedziału
+    if abs(f(x)) < e:         # Jeśli wartość funkcji w punkcie x jest dostatecznie bliska zeru, to znaleźliśmy pierwiastek
+        yo = f(x)             # Zapisujemy wartość funkcji w punkcie x jako yo
+        xo = x                # Zapisujemy wartość x jako xo
+        break                 # Przerywamy pętlę
+    elif f(a) * f(x) < 0:     # Jeśli wartości funkcji w punktach a i x mają różne znaki, to pierwiastek znajduje się w przedziale [a, x]
+        b = x                 # Ustawiamy końcową wartość przedziału jako x
+    else:                     # W przeciwnym przypadku pierwiastek znajduje się w przedziale [x, b]
+        a = x                 # Ustawiamy początkową wartość przedziału jako x
 
-# Отображаем найденный корень на графике
-plt.plot(xo, yo, 'ro')
+plt.plot(xo, yo, 'ro')        # Rysujemy punkt na wykresie, który odpowiada znalezionemu pierwiastkowi
 
-# Отображаем график функции и найденного корня
-plt.show()
+plt.show()                     # Wyświetlamy wykres
--- mnt/lab_2/4.py
+++ mnt/lab_2/4.py
@@ -1,30 +1,30 @@
 import numpy as np
 
-def funkcja(x):
-    return (np.exp(5*x)/100) - 1/4
+def funkcja(x): # funkcja do której szukamy pierwiastka
+    return (np.exp(5*x)/100) - 1/4 
 
-def pochodna(x):
+def pochodna(x): # pochodna funkcji
     return 5*np.exp(5*x)/100
 
-def metoda_newtona(x0, delta):
+def metoda_newtona(x0, delta): # metoda Newtona
     iteracje = 0
     x = x0
-    while abs(funkcja(x)) > delta:
-        x = x - funkcja(x) / pochodna(x)
-        iteracje += 1
+    while abs(funkcja(x)) > delta: # warunek kończący iteracje, jeśli znaleziono pierwiastek z dostatecznie małą dokładnością
+        x = x - funkcja(x) / pochodna(x) 
+        iteracje += 1 
     pierwiastek = x
-    return pierwiastek, iteracje
+    return pierwiastek, iteracje # zwracamy znaleziony pierwiastek i liczbę iteracji
 
-delta = 1e-5
-x0_1 = 0.05
-x0_2 = 0.9
+delta = 1e-5 # dokładność szukania pierwiastka
+x0_1 = 0.05 # punkt startowy algorytmu
+x0_2 = 0.9 # punkt startowy algorytmu
 
-pierwiastek_1, iteracje_1 = metoda_newtona(x0_1, delta)
-print("Dla x0 = 0.05:")
-print("Pierwiastek: ", pierwiastek_1)
+pierwiastek_1, iteracje_1 = metoda_newtona(x0_1, delta) # wywołujemy funkcję
+print("Dla x0 = 0.05:") # wypisujemy wyniki
+print("Pierwiastek: ", pierwiastek_1) # wypisujemy wyniki
 print("Liczba iteracji: ", iteracje_1)
 
-pierwiastek_2, iteracje_2 = metoda_newtona(x0_2, delta)
-print("Dla x0 = 0.9:")
-print("Pierwiastek: ", pierwiastek_2)
-print("Liczba iteracji: ", iteracje_2)
+pierwiastek_2, iteracje_2 = metoda_newtona(x0_2, delta) # wywołujemy funkcję
+print("Dla x0 = 0.9:") # wypisujemy wyniki
+print("Pierwiastek: ", pierwiastek_2) 
+print("Liczba iteracji: ", iteracje_2) 
